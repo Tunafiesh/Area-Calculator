b2eb04b2e013f2221a5c011694dcda190d5f5e84
Tunafiesh <tylercakotc@gmail.com>
2019-04-28 23:24:25 -0600

    Updated :wg

--- js/main.js
+++ js/main.js
@@ -8,4 +8,40 @@ const rectangle = document.getElementById('rectangle');
 const triangle = document.getElementById('triangle');
 
 
+// Event Listeners
+square.addEventListener('click', squareCalculator);
 
+rectangle.addEventListener('click', rectangleCalculator);
+
+triangle.addEventListener('click', triangleCalculator);
+
+// Square Selector
+function squareCalculator() {
+    clearBtn()
+    if(square.classList.contains = 'btn white-text waves-effect'){
+    square.className += ' active-link z-depth-0'
+    }
+};
+
+// Rectangle Selector
+function rectangleCalculator() {
+    clearBtn()
+    if(rectangle.classList.contains = 'btn white-text waves-effect'){
+    rectangle.className += ' active-link z-depth-0'
+    }
+};
+
+// Triangle Selector
+function triangleCalculator() {
+    clearBtn()
+    if(triangle.classList.contains = 'btn white-text waves-effect'){
+    triangle.className += ' active-link z-depth-0'
+    }
+};
+
+// Clear Button Selection
+function clearBtn(){
+    square.className = 'btn white-text waves-effect'
+    rectangle.className = 'btn white-text waves-effect'
+    triangle.className = 'btn white-text waves-effect'
+};
