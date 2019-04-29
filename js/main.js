84156e2f17ee96f2e4e675dbf112f5803849c448
Tunafiesh <40344598+Tunafiesh@users.noreply.github.com>
2019-04-29 09:58:53 -0600

    Updated Javascript :wq

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const shapeSelector = document.getElementById('shape-selector');
 const calculator = document.getElementById('calculator');
 const calculate = document.getElementById('calculate');
 const calculation = document.querySelector('.calculation');
+const calculationBtn = document.getElementById('calculate-btn');
 
 const square = document.getElementById('square');
 const rectangle = document.getElementById('rectangle');
@@ -14,6 +15,8 @@ square.addEventListener('click', squareCalculator);
 rectangle.addEventListener('click', rectangleCalculator);
 
 triangle.addEventListener('click', triangleCalculator);
+
+calculationBtn.addEventListener('click', calculating)
 
 // Square Selector
 function squareCalculator() {
@@ -45,3 +48,15 @@ function clearBtn(){
     rectangle.className = 'btn white-text waves-effect'
     triangle.className = 'btn white-text waves-effect'
 };
+
+// Calculate Area
+function calculating(e) {
+    let base = document.getElementById('base').value
+
+    console.log(base);
+    if(square.classList.contains = 'active-link') {
+
+    }
+    
+    e.preventDefault();
+}
